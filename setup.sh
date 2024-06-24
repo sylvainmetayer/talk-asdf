e12ec636bdff395491ab7aa29ffe72af638a23e5
Sylvain <s.metayer@groupeonepoint.com>
2024-06-24 13:53:02 +0200

    feat: breizhcamp

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ curl -sLo code/demo-magic.sh https://raw.githubusercontent.com/paxtonhare/demo-m
 git clone https://github.com/hakimel/reveal.js --depth 1 --branch 5.0.5
 git clone https://github.com/denehyg/reveal.js-menu --depth 1 --branch 2.1.0 reveal.js/plugin/menu
 cd reveal.js && npm ci && npm run build && cd ../
-git clone git@github.com:highlightjs/highlight.js.git --depth=1 -b 11.9.0
+git clone https://github.com/highlightjs/highlight.js --depth=1 -b 11.9.0
 cd highlight.js && npm ci && npm run build-cdn && cp -r build/highlight.min.js build/styles ../js/highlight
